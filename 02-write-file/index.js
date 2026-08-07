1e800b069b7a6a4aa23a4bf3b9393aa9c054df34
Oardieldei <alik.want.you@gmail.com>
2026-08-07 11:58:51 +0300

    feat: add solution for 2nd task

---
+++ 02-write-file/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+const path = require('path')
+const readline = require('readline')
+
+const filePath = path.join(__dirname, 'text.txt')
+
+const writeStream = fs.createWriteStream(filePath, { flags: 'a' })
+
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout })
+
+console.log('Введите текст. Для выхода напишите "exit" или нажмите Ctrl + C.')
+
+rl.on('line', (input) => {
+  if (input.trim() === 'exit') {
+    exitProgram()
+    return
+  }
+
+  writeStream.write(`${input}\n`)
+})
+
+process.on('SIGINT', () => {
+  exitProgram()
+})
+
+function exitProgram() {
+	rl.close()
+  writeStream.end()
+
+  process.exit()
+}
